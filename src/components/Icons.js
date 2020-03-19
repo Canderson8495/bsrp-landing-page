50d5e7fa14374d1812c7f5c467117ed04ee79d60
Erryial <Thrillechs@gmail.com>
2020-03-19 17:17:35 -0400

    Added InterestForm component ability to communicate to database with nodejs

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -2,34 +2,34 @@ import React from 'react';
 
 
 export const Icons = () => (
-  <section class="features-icons bg-light text-center">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-4">
-          <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
-            <div class="features-icons-icon d-flex">
-              <i class="icon-screen-desktop m-auto text-primary"></i>
+  <section className="features-icons bg-light text-center">
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-4">
+          <div className="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+            <div className="features-icons-icon d-flex">
+              <i className="icon-screen-desktop m-auto text-primary"></i>
             </div>
             <h3>Great Performance</h3>
-            <p class="lead mb-0">Server resources built with performance at the top of our priorities!</p>
+            <p className="lead mb-0">Server resources built with performance at the top of our priorities!</p>
           </div>
         </div>
-        <div class="col-lg-4">
-          <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
-            <div class="features-icons-icon d-flex">
-              <i class="icon-layers m-auto text-primary"></i>
+        <div className="col-lg-4">
+          <div className="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+            <div className="features-icons-icon d-flex">
+              <i className="icon-layers m-auto text-primary"></i>
             </div>
             <h3>Custom Scripts</h3>
-            <p class="lead mb-0">Custom-Built scripts that you won't be able to find anywhere else! </p>
+            <p className="lead mb-0">Custom-Built scripts that you won't be able to find anywhere else! </p>
           </div>
         </div>
-        <div class="col-lg-4">
-          <div class="features-icons-item mx-auto mb-0 mb-lg-3">
-            <div class="features-icons-icon d-flex">
-              <i class="icon-check m-auto text-primary"></i>
+        <div className="col-lg-4">
+          <div className="features-icons-item mx-auto mb-0 mb-lg-3">
+            <div className="features-icons-icon d-flex">
+              <i className="icon-check m-auto text-primary"></i>
             </div>
             <h3>Active Staff</h3>
-            <p class="lead mb-0">Active Staff and Developers that will give you the best possible experience!</p>
+            <p className="lead mb-0">Active Staff and Developers that will give you the best possible experience!</p>
           </div>
         </div>
       </div>
